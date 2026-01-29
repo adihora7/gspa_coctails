630e93a5f7598e04fb68d5c7553a8041df3ffa25
adihora7 <aadityadihora7@gmail.com>
2026-01-29 00:07:08 -0500

    Implement the firs part of the hero section

--- Cocktails/constants/index.js
+++ Cocktails/constants/index.js
@@ -110,8 +110,8 @@ const storeInfo = {
 };
 
 const openingHours = [
- { day: "Mon–Thu", time: "11:00am – 12am" },
- { day: "Fri", time: "11:00am – 2am" },
+ { day: "Mon–Fri", time: "11:00am – 12am" },
+ ///{ day: "Fri", time: "11:00am – 2am" },
  { day: "Sat", time: "9:00am – 2am" },
  { day: "Sun", time: "9:00am – 1am" },
 ];
